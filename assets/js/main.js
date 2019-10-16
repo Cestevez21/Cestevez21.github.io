8b54fec7d7e4fcc770d24317bf6a97e97a6d4223
Cestevez21 <00120218@uca.edu.sv>
2019-10-15 20:28:02 -0600

    Fix: Errores menores

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,12 @@ var show = post[0];
 
 window.onload = () => {
 	console.log(show)
-  for (let i = 0; i < 6; i++) {
-    menu[i].addEventListener("click", () => {
-      show.classList.add("hide-post");
-      show = post[i];
-	  show.classList.remove("hide-post");
-	  console.log(show)
-    });
-  }
+	for (let i = 0; i < 6; i++) {
+		menu[i].addEventListener("click", () => {
+			show.classList.add("hide-post");
+			show = post[i];
+			show.classList.remove("hide-post");
+			console.log(show)
+		});
+	}
 };
-
